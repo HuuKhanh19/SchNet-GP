f622cf571ddc3a4e5c2a77c60714652d0da490bc
HuuKhanh19 <hkhanhp19@gmail.com>
2026-06-02 14:03:12 +0700

    dung updat

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,5 @@
 """Models module."""
 from .schnet import SchNet, build_schnet_model
-from .pretrained import load_pretrained_qm9_backbone
-
 __all__ = [
-    'SchNet', 'build_schnet_model', 'load_pretrained_qm9_backbone',
+    'SchNet', 'build_schnet_model'
 ]
